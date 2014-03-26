088728b1238ddde000cd406d7078eb4cb731ccc3
uo219560 <uo219560@EII-L-02B-04.aulas.uniovi.es>
2014-03-26 09:55:51 +0100

    Creacion de una historia de usuario "AccesoDatos" (Cucumber)

---
+++ src/test/java/es/uniovi/asw/observaTerra/cucumber/AccesoDatosSteps.java
@@ -0,0 +1,25 @@
+package es.uniovi.asw.observaTerra.cucumber;
+
+import org.apache.commons.lang.NotImplementedException;
+
+import cucumber.api.java.es.Dada;
+import cucumber.api.java.es.Entonces;
+
+/**
+ * Como consumidor quiero acceder a la información del sistema ObservaTerra para obtener recursos.
+ *
+ * @author uo219560
+ *
+ */
+public class AccesoDatosSteps {
+	
+	@Dada("la conexion al sistema$")
+	public void la_conexion_al_sistema() throws Throwable{
+		throw new NotImplementedException();
+	}
+	
+	@Entonces("recibimos respuesta en menos de (.+) segundos$")
+	public void recibimos_respuesta_en_menos_de_segundos(double segundos) throws Throwable{
+		throw new NotImplementedException();
+	}
+}
---
+++ src/test/resources/es/uniovi/asw/observaTerra/cucumber/Acceso.feature
@@ -0,0 +1,9 @@
+# language: es
+Característica: Respuesta minima
+
+Escenario: Respuesta por parte del servidor en tiempo admisible
+
+
+    
+     
+    
