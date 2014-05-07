9d7895010a1f975606317bbcacf9ae197eca2a94
fernandoj92 <fernandoj92@gmail.com>
2014-05-07 17:54:44 +0200

    Actualización API
    Se ha añadido la posibilidad de añadir observaciones desde el exterior
    mediante API (crawlers externos y demas...)

--- app/controllers/API.java
+++ app/controllers/API.java
@@ -6,6 +6,11 @@ import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import javax.persistence.PersistenceException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Observaterra's API (Only Json right now)
  *
@@ -27,5 +32,33 @@ public class API extends Controller{
 
     public static Result observationsByArea(String areaName){
         return ok(Json.toJson(Observacion.allByArea(areaName)));
+    }
+
+    public static Result addObservation(String providerName, String indicatorName,String areaName,String measure,String value,String initialDate,String endDate) {
+        //First we need to transform this Strings into their true types
+
+        //Area (riht now we only accept countries, so every area is a country...)
+        Country country = new Country(areaName.toLowerCase());
+        try {
+            Country.create(country);
+        }catch(PersistenceException p){}//We do nothing, we simply avoid that Country creation
+
+        //Value
+        int intValue = Integer.parseInt(value);//Will throw Exception if the value is not a real integer...
+
+        //Date
+        SimpleDateFormat textFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date InitDate = null;
+        Date EndDate = null;
+
+        try {
+            InitDate = textFormat.parse(initialDate);
+            EndDate = textFormat.parse(endDate);
+        } catch (ParseException ex) { ex.printStackTrace();} //We should notify the person who is adding Observations
+
+        Observacion.create(providerName,indicatorName,country,measure,intValue,InitDate,EndDate);
+
+        //We return de Json observations of that area
+        return observationsByArea(areaName);
     }
 }
